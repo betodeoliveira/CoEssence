17aa752453fe534ec0f83eda164a0b49405214ba
Alberto de Olivira <betodeoliveira@outlook.com>
2023-03-21 11:27:08 -0300

    Coaching Details

--- dashed-lines-coaching.js
+++ dashed-lines-coaching.js
@@ -6,6 +6,49 @@ createScrollTrigger(".dashed-line-embed.is-coaching-message",
     null,
     null);
 
+// Details Title
+createScrollTrigger(".dashed-line-embed.is-coaching-details-title",
+    "#maskPath-coachinngDetailsTitle",
+    true,
+    ".section-content.is-coaching-details-title",
+    null,
+    null);
+
+// Match Media - On desktop
+matchMedia.add("(min-width: 992px)", () => {
+    // Details 01
+    createScrollTrigger(".dashed-line-embed.is-coaching-details-dash1",
+        "#maskPath-coachingDetailsDash1",
+        true,
+        ".coaching-details_option-item.is-01",
+        null,
+        null);
+
+    // Details 02
+    createScrollTrigger(".dashed-line-embed.is-coaching-details-dash2",
+        "#maskPath-coachingDetailsDash2",
+        true,
+        ".coaching-details_option-item.is-02",
+        null,
+        null);
+
+    // Details 03
+    createScrollTrigger(".dashed-line-embed.is-coaching-details-dash3",
+        "#maskPath-coachingDetailsDash3",
+        true,
+        ".coaching-details_option-item.is-03",
+        null,
+        null);
+
+    // Details 04
+    createScrollTrigger(".dashed-line-embed.is-coaching-details-dash4",
+        "#maskPath-coachingDetailsDash4",
+        true,
+        ".coaching-details_option-item.is-04",
+        null,
+        null);
+});
+
 // Embed
 createScrollTrigger(".dashed-line-embed.is-coaching-embed",
     "#maskPath-coachingEmbed",
