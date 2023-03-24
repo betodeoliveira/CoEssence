912cd40067c0af4d6360286d94da9f78cb8f53da
Alberto de Olivira <betodeoliveira@outlook.com>
2023-03-24 11:33:48 -0300

    Retreats new sections

--- dashed-lines-retreats.js
+++ dashed-lines-retreats.js
@@ -1,8 +1,24 @@
-// Message
-createScrollTrigger(".dashed-line-embed.is-retreats-message",
-    "#maskPath-retreatsMessage",
+// Message 01
+createScrollTrigger(".dashed-line-embed.is-retreats-message-01",
+    "#maskPath-retreatsMessage01",
     true,
-    ".section-content.is-retreats-message",
+    ".section-content.is-retreats-message-01",
+    null,
+    null);
+
+// Message 02
+createScrollTrigger(".dashed-line-embed.is-retreats-message-02",
+    "#maskPath-retreatsMessage02",
+    true,
+    ".section-content.is-retreats-message-02",
+    null,
+    null);
+
+// Gallery
+createScrollTrigger(".dashed-line-embed.is-retreats-gallery",
+    "#maskPath-retreatsGallery",
+    true,
+    ".section-content.is-retreats-gallery",
     null,
     null);
 
